4a789161747a69b6e99fe6d20d453bf04211ada3
TimRoith <44805883+TimRoith@users.noreply.github.com>
2019-02-19 00:10:03 +0100

    Add files via upload
    The data structure for the edges (and weights) has been changed:
    
    before: Array{Int64, 2} 
    
    now:  Array{Array{Int64, 1}, 1}
    
    This was done, because it omits th storage of a list with indices of the node neighbors.
    Accesing and constructing an array of this type is pretty much identically as it was before (see testScripts/VarGraphPerformance.jl)
    
    Furthermore the BallGraph-functionality was added.

--- VariationalGraph/VarGraph.jl
+++ VariationalGraph/VarGraph.jl
@@ -1,7 +1,7 @@
 module VarGraph
 using SparseArrays
 using NearestNeighbors
-import NearestNeighbors: KDTree, BallTree, BruteTree, knn
+import NearestNeighbors: KDTree, BallTree, BruteTree, knn, inrange
 
 export constructGraph, getadjMat, undirect
 
@@ -14,12 +14,12 @@ A type representing an directed weighted Graph graph.
 struct VariationalGraph{T<:Integer, U<:Real}
     num_verts::Int
     num_edges::Int
-    edges::Array{T,2}
-    weights::Array{U,1}
-    function VariationalGraph{T, U}(num_verts, edges, weights) where T<:Integer where U<:Real
-        num_edges = size(edges, 1)
-        new{T, U}(num_verts, num_edges, edges, weights)
-    end
+    edges::Array{Array{T, 1}, 1}
+    weights::Array{Array{U, 1}, 1}
+#     function VariationalGraph{T, U}(num_verts, edges, weights) where T<:Integer where U<:Real
+#         num_edges = size(edges, 1)
+#         new{T, U}(num_verts, num_edges, edges, weights)
+#     end
 end
 #------------------------------------------------
 VariationalGraph(nv::Int, edges::Array{T,2}, weights::Array{U,1}) where T<:Integer where U<:Real = 
@@ -33,8 +33,10 @@ function getadjMat(g::VariationalGraph)
     #A = Array{typeof(g.weights[1])}(undef,g.num_verts, g.num_verts)
     A = spzeros(g.num_verts, g.num_verts)
     e = g.edges
-    for u = 1:g.num_edges
-        A[e[u,1],e[u,2]] = g.weights[u]
+    for u = 1:g.num_verts
+        for v = 1:length(e[u])
+            A[u,e[u][v]] = g.weights[u][v]
+        end
     end
     return A
 end
@@ -47,19 +49,18 @@ function undirect(g::VariationalGraph)
     A = getadjMat(g)
     A = 0.5 * (A + transpose(A))
     num_edges = nnz(A)
-    edges = Array{Int64}(undef, num_edges, 2)
-    weights = Array{Float64}(undef, num_edges)
+    edges = [Vector{Int64}() for _ in 1:g.num_verts]
+    weights = [Vector{Float64}() for _ in 1:g.num_verts]
     cols = A.colptr
     u = 1
     for i = 1:(length(cols) - 1)
         for j = cols[i]:(cols[i + 1] - 1)
-            edges[j, 1] = u
-            edges[j, 2] = A.rowval[j]
-            weights[j] = A.nzval[j]
+            push!(edges[u],A.rowval[j])
+            push!(weights[u],A.nzval[j]) 
         end
         u += 1
     end
-    VariationalGraph(g.num_verts, edges, weights)
+    VariationalGraph(g.num_verts, num_edges, edges, weights)
 end
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -93,27 +94,16 @@ function constructGraph(data::Dict, ngh::Dict, dist_fcts::Dict, weight_fcts::Dic
     # knn - PointCloud
     elseif lowercase(ngh["nType"]) == "knn"
         # Extract given input
-        num_verts = div(length(data["f"]),data["dimRange"])
+        # num_verts = div(length(data["f"]),data["dimRange"])
+        num_verts = size(data["points"], 2)
         num_nghs = ngh["num_nghs"]
         inner_norm = dist_fcts["inner_norm"]
         outer_norm = dist_fcts["outer_norm"]
         weight_fct = weight_fcts["fct"]
-        # Initialize the neighbors of nodes
-        node_nghs = Array{Int64}(undef, num_verts, 3) 
-        for i = 1:num_verts
-            node_nghs[i, 1] = i
-            node_nghs[i, 2] = (i - 1) * num_nghs + 1
-            node_nghs[i, 3] = num_nghs
-        end
         # Set number of edges
         num_edges = num_verts * num_nghs;
-        # Initialize Array for Edges and Edgeweights
-        edges = Array{Int64}(undef,num_edges,2)
-        weights = Array{Float64}(undef, num_edges)
-        # Set first column of edges to node indices
-        for i = 0:(num_edges-1)
-            edges[i + 1, 1] = div(i + num_nghs,num_nghs)
-        end
+        # Initialize Array for Edgeweights
+        weights = [Vector{Float64}(undef, num_nghs) for _ in 1:num_verts]
         # Check the given DataType
         if lowercase(data["type"]) == "grid"
             error("Currently unsupported!")
@@ -123,25 +113,58 @@ function constructGraph(data::Dict, ngh::Dict, dist_fcts::Dict, weight_fcts::Dic
             kdtree = KDTree(points);
             # Extraxt neighbors for each vertex via knn algorithm
             # One can obtain a second output "dists", which might be useful
-            inds, = NearestNeighbors.knn(kdtree, points, num_nghs + 1, false)
+            edges, = NearestNeighbors.knn(kdtree, points, num_nghs + 1, false)
             # Modify the obtained inds, store them into the edge array and 
             # calculate the dists
             for u = 1:num_verts
                 # delete the index of u itself in the inds array
-                setdiff!(inds[u],u)
+                setdiff!(edges[u],u)
                 pt = points[:,u]
                 # set edges and weights accordingly
                 for v = 1:num_nghs
-                    edges[(u - 1) * num_nghs + v, 2] = inds[u][v]
                     # Set weights via outer and inner Norm and weight function
-                    inner_dist = inner_norm(pt - points[:,inds[u][v]])
-                    weights[(u - 1) * num_nghs + v] = weight_fct(outer_norm(inner_dist[1,1] + inner_dist[2,1]))
+                    inner_dist = inner_norm(pt - points[:,edges[u][v]])
+                    weights[u][v] = weight_fct(outer_norm(inner_dist[1,1] + inner_dist[2,1]))
                 end
-
             end
         end
-        # Construct the corresponding Graph
-        g = VariationalGraph(num_verts, edges, weights)
+        g = VariationalGraph(num_verts, num_edges, edges, weights)
+    elseif lowercase(ngh["nType"]) == "epsilon"
+        # Extract given input
+        # num_verts = div(length(data["f"]),data["dimRange"])
+        num_verts = size(data["points"], 2)
+        epsilon = ngh["epsilon"]
+        inner_norm = dist_fcts["inner_norm"]
+        outer_norm = dist_fcts["outer_norm"]
+        weight_fct = weight_fcts["fct"]
+        # Initialize Array for Edgeweights
+        weights = [Vector{Float64}() for _ in 1:num_verts]
+        # Check the given DataType
+        if lowercase(data["type"]) == "grid"
+            error("Currently unsupported!")
+        elseif lowercase(data["type"]) == "point_cloud"
+            # Generate ball-Tree from given Data
+            points = data["points"]
+            kdtree = BallTree(points);
+            # Extraxt neighbors for each vertex via a range algorithm
+            edges = NearestNeighbors.inrange(kdtree, points, epsilon, false)
+            # Modify the obtained inds, store them into the edge array and 
+            # calculate the dists
+            num_edges = 0
+            for u = 1:num_verts
+                # delete the index of u itself in the inds array
+                setdiff!(edges[u],u)
+                pt = points[:,u]
+                # set edges and weights accordingly
+                for v = 1:length(edges[u])
+                    # Set weights via outer and inner Norm and weight function
+                    inner_dist = inner_norm(pt - points[:,edges[u][v]])
+                    push!(weights[u], weight_fct(outer_norm(inner_dist[1,1] + inner_dist[2,1])))
+                    num_edges += 1
+                end
+            end
+        end
+        g = VariationalGraph(num_verts, num_edges, edges, weights)
     else
         error("Unsupported neighborhood type!")
     end
--- VariationalGraph/VarGraph_test_point_cloud.jl
+++ VariationalGraph/VarGraph_test_point_cloud.jl
@@ -2,23 +2,25 @@ include("VarGraph.jl")
 using.VarGraph
 #------------------------------------------------
 # This takes some time, include only for visualization
-include("GraphVis.jl")
-using.GraphVis
+# include("GraphVis.jl")
+# using.GraphVis
 #------------------------------------------------
 # Visualization works for d = 2,3 
 # Calculation should work for "arbitrary" positive integers
 dim_domain = 2
-num_nghs = 4
+num_nghs = 3
+num_pts = 10
+epsilon = 1
 #------------------------------------------------
-const points = rand(dim_domain,30)
-const F = rand(30)
+const points = rand(dim_domain, num_pts)
+const F = rand(num_pts)
 #------------------------------------------------
 inner_norm(x) = x.^2
 outer_norm(x) = sqrt(x)
 w_fct(x) = 1 ./ (x.^2)
 f() = "dummy"
 #------------------------------------------------
-ngh = Dict([("nType", "knn"), ("direction", "full"), ("searchRadius", 1), ("num_nghs", num_nghs)])
+ngh = Dict([("nType", "epsilon"), ("direction", "full"), ("searchRadius", 1), ("num_nghs", num_nghs), ("epsilon", epsilon)])
 distFct = Dict([("inner_norm", inner_norm), ("outer_norm", outer_norm)])
 wFct = Dict([("sigma", 0), ("fct", w_fct)])
 data = Dict([("type", "point_cloud"), ("dimDomain", dim_domain), ("dimRange", 1), ("f", F),("points", points)])
@@ -26,9 +28,10 @@ data = Dict([("type", "point_cloud"), ("dimDomain", dim_domain), ("dimRange", 1)
 display("Starting to calculate!")
 G = constructGraph(data,ngh,distFct,wFct)
 #------------------------------------------------
+J = getadjMat(G)  
 H = undirect(G)
-display("Starting to plot! Have a coffee in the meantime ;)")
-vis(H.edges, points) 
+# display("Starting to plot! Have a coffee in the meantime ;)")
+# vis(H.edges, points) 
 #------------------------------------------------
 # A = [3 0 0 0; 0 5 0 0; 2 2 0 0; 0 0 2 0]
 # display("Starting to plot")
