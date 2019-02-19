a9c9002c99b07d01797d23dbd80e2301626a274d
Roith <aj31ikug@cip-54-58.mi.uni-erlangen.de>
2019-02-19 19:38:05 +0100

    We don't need that anymore, the VariationalGraphs directory is better!

--- VariationalGraph/GraphVis_test.jl
+++
@@ -1,9 +0,0 @@
-include("GraphVis.jl")
-using.GraphVis
-display("finished loading stuff")
-#------------------------------------------------
-num_points = 30
-#------------------------------------------------
-const points = rand(2,num_points)
-const edges = Int64[rand(1:num_points) for i in 1:(2 * num_points), j in 1:2]
-vis(edges,points)
