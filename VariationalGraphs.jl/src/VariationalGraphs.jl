e9385a90af54b7d545429ed9089074d6375c4e1a
Roith <aj31ikug@cip-54-32.mi.uni-erlangen.de>
2019-02-21 14:42:29 +0100

    Update dependencies

--- VariationalGraphs.jl/src/VariationalGraphs.jl
+++ VariationalGraphs.jl/src/VariationalGraphs.jl
@@ -2,17 +2,24 @@ module VariationalGraphs
 
 using SparseArrays
 using NearestNeighbors
+using LightGraphsFlows
 
+import LightGraphsFlows: 
+mincut
+import LightGraphs:
+DiGraph, add_edge!
 
 import NearestNeighbors: 
 KDTree, BallTree, BruteTree, knn, inrange
 
-export AbstractVariationalGraph, constructGraph, getadjMat, undirect
+export AbstractVariationalGraph, constructGraph, getadjMat, undirect,
+generate_flowgraph
 
 abstract type AbstractVariationalGraph{T<:Integer, U<:Real} end
 
 include("./knn_graph.jl")
 include("./epsilon_graph.jl")
 include("./variationalgraph.jl")
+include("./generate_flowgraph.jl")
 
 end # module
